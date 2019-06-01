966b9616c6e14996027bd19127f13b5b2acc551c
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-06-01 12:47:14 -0400

    Sobrepondo a variável de estado

--- src/index.js
+++ src/index.js
@@ -3,40 +3,38 @@ import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
-
   static defaultProps = {
     children: "Salvar"
   };
-  
+
   static propTypes = {
     onClick: PropTypes.func.isRequired,
     children: PropTypes.string
   };
 
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
 class App extends Component {
-  handleClick() {
-    alert("Botão clicado");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    // estamos sobrepondo a variável no estado, pois, não é permitido alterar o valor dela
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
   render() {
+    console.log("render");
+
     return (
       <Fragment>
         <h1>Hello World!</h1>
-        <Button
-          onClick={() => {
-            alert("Button 1");
-          }}
-        />
-        <Button onClick={this.handleClick}> Enviar </Button>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}> Somar </Button>
       </Fragment>
     );
   }
